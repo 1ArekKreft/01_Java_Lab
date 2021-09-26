e0529c5d8652f032283f581d3751f9f9611f8486
Kenziexe <1areknetflix@gmail.com>
2021-09-26 13:08:31 +0200

    test23

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,17 @@ public class Main {
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int liczba = 4;
+        String slowo = "test";
+        char znak = 'x';
+        double cyfra = 4.555555555;
+        float floatcyfra= 5.32f;
 
+        System.out.println(liczba);
+        System.out.println(slowo);
+        System.out.println(znak);
+        System.out.println(cyfra);
+        System.out.println(floatcyfra);
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
         *    mnożenia, dziealenia i modulo na zmiennych:
