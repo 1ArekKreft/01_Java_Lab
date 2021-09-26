2c4f516d9828977ba66ee1fd1f1c938df0be03f0
Kenziexe <1areknetflix@gmail.com>
2021-09-26 13:04:53 +0200

    test23

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        int liczba = 2;
+        float cyfra = 5.4f;
         System.out.println("Testowa wiadomosc");
         System.out.print("Testowa wiadomosc");
+        System.out.printf("Testowa %s wiadomosc, float =  %f","wiec",cyfra);
 
 
     }
