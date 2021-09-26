2d89549aa630d4d938996f0de0d4f6125908b335
Kenziexe <1areknetflix@gmail.com>
2021-09-26 12:58:24 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Testowa wiadomosc");
+        System.out.print("Testowa wiadomosc");
 
 
     }
