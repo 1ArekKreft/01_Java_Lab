ed0026d9710b6bd84cb1239815d722d876a2432d
1ArekKreft <arkadiuszkreft32@gmail.com>
2022-01-11 18:07:10 +0100

    z

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,25 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznychh */
 
+        int liczba = 14;
+
+        //Arytmetyczne
+
+        if(liczba%2==0 && liczba>0)
+        {
+            System.out.println("Podana liczba jest większa od zera i jest parzysta");
+        }
+        System.out.println();
+
+        if(liczba>10 && liczba<101)
+        {
+            System.out.println("Podana liczba mieści się w przedziale od 10 do 100");
+        }
+        System.out.println();
+
+        if(liczba % 2 == 0 && liczba % 6 ==0)
+        {
+            System.out.println("Podana liczba jest podzielna przez 2 i 6");
+        }
     }
 }
